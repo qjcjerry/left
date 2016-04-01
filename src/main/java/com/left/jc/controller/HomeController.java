cca0978115d5fc03b1828b1141c51cccaa16a729
曹全军 <caoquanjun@meituan.com>
2016-04-01 10:58:37 +0800

    add html parsing

--- src/main/java/com/left/jc/controller/HomeController.java
+++ src/main/java/com/left/jc/controller/HomeController.java
@@ -4,6 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * Created by chenkaihua on 15-12-15.
  */
@@ -17,5 +22,10 @@ public class HomeController {
         return new ModelAndView("index");
     }
 
-
+    @RequestMapping(value="/*.html")
+    public void htmlView(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // ...
+//      request.getRequestDispatcher("index.html").forward(request, response);
+        response.sendRedirect(request.getContextPath()+"/html"+request.getServletPath());
+    }
 }
--- src/main/java/com/left/jc/mapper/UserMapper.java
+++ src/main/java/com/left/jc/mapper/UserMapper.java
@@ -1,9 +1,7 @@
 package com.left.jc.mapper;
 
 import com.left.jc.model.User;
-import org.springframework.stereotype.Repository;
 import tk.mybatis.mapper.common.Mapper;
 
-//@Repository
 public interface UserMapper extends Mapper<User> {
 }
